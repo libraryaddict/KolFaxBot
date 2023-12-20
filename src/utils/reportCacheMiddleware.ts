fb91c18cd552d96b7d0e45de06752900fff13415
libraryaddict <github@lib.co.nz>
2023-12-20 16:54:25 +1300

    Cache header

--- src/utils/reportCacheMiddleware.ts
+++ src/utils/reportCacheMiddleware.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "@tinyhttp/app";
 
 const cache = new Map<string, string>();
+const cacheHeaders = new Map<string, string>();
 
 export const invalidateReportCache = () => void cache.clear();
 
@@ -12,6 +13,10 @@ export const cacheReports = () => {
       const value = cache.get(key);
 
       if (value) {
+        if (cacheHeaders.has(key)) {
+          res.header("Content-Type", cacheHeaders.get(key));
+        }
+
         res.send(value);
 
         return;
@@ -21,6 +26,10 @@ export const cacheReports = () => {
       const _send = res.send;
 
       res.send = (body: string) => {
+        if (typeof res.getHeader("Content-Type") == "string") {
+          cacheHeaders.set(key, res.getHeader("Content-Type") as string);
+        }
+
         cache.set(key, body);
 
         return _send(body);
